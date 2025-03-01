629d33e36ca7ff6eaab1b2774acc688b689f58e8
LV <vamshi.lagishetty24@gmail.com>
2025-03-01 20:01:46 +0530

    created signin route

--- api/routes/userApp.js
+++ api/routes/userApp.js
@@ -1,30 +1,83 @@
 const express = require("express");
 const User = require("../models/userModal");
 const userApp = express.Router();
-const bcryptjs = require("bcryptjs")
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { errorHandler } = require("../utils/error.js");
 
 
-
-// Define routes
-userApp.get("/test", async (req, res,next) => {
-    res.send({ message: "test route" });
+// Test route
+userApp.get("/test", async (req, res) => {
+  res.send({ message: "test route" });
 });
 
-userApp.post("/signup",async(req,res,next)=>{
-    const { username, email, password } = req.body;
-    
-    const hashedPassword = bcryptjs.hashSync(password,10)
+// Signup route
+userApp.post("/signup", async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    const newUser=new User({
-        username,email,password:hashedPassword,
-    })
-
-    try {
-        await newUser.save();
-        res.json('Signup successful');
-      } catch (error) {
-        next(error)
+  try {
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return next(errorHandler(400, "Email is already in use"));
     }
-})
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Signup successful" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Signin route
+userApp.post("/signin", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate input
+  if (!email || !password) {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    // Find user by email
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    // Validate password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // Exclude password from response
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = userApp;
